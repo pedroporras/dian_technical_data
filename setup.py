71ba61545dc0d3d1a5a2a37971ec58e1b95e5c5b
Pedro Porras <pedro.porras@nmksoftware.co>
2021-10-28 00:12:01 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "DIANtechnicaldata"
+
+setup(
+    name=PACKAGE_NAME,
+    version='0.1.0',
+    description='Modulo with DIAN data definition',
+    url='',
+    author='Pedro Porras',
+    author_email='fmpedroporras@gmail.com',
+    license='LGPL',
+    packages=['DIANtechnicaldata'],
+)

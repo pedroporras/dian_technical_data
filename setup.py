e1f019d4b7963e96161fc4c942cfa36ee61f01be
Pedro Porras <pedro.porras@nmksoftware.co>
2021-11-05 23:07:57 -0500

    Adicion de paquete para publicacion

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
-PACKAGE_NAME = "DIANtechnicaldata"
+VERSION = '0.1.0'
+DESCRIPTION = 'Modulo con definiciones de la DIAN'
+LONG_DESCRIPTION = 'Paquete de python para manejar la informacion de los anexos tecnicos de la DIAN en Colombia'
+
+PACKAGE_NAME = "Docedian"
 
 setup(
     name=PACKAGE_NAME,
     version='0.1.0',
-    description='Modulo with DIAN data definition',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     url='',
     author='Pedro Porras',
-    author_email='fmpedroporras@gmail.com',
+    author_email='<pedroporras@inexpresivo.com>',
+    keywords=['DIAN', 'Colombia'],
     license='LGPL',
-    packages=['DIANtechnicaldata'],
+    packages=find_packages(),
 )

a7cc90e5e3794fc24d510eaab29e04f2bb273825
Pedro Porras <pedro.porras@nmksoftware.co>
2022-05-19 13:44:37 -0500

    feat: adicion de calculo de digito de verificacion

--- docedian/base/base.py
+++ docedian/base/base.py
@@ -17,3 +17,28 @@ TipoDocumento = {
     '50': '50 - NIT de otro país',
     '91': '91 - NUIP * ',
 }
+
+
+class Dianbase():
+
+    @classmethod
+    def compute_check_digit(cls, rut: str, identification_type: str) -> str:
+        """
+        @param rut(str): Rut without check digit
+        @param identification_type(str): Identification type
+
+        @return result(str): Return the check digit of the rut
+        """
+        result = 0
+        if identification_type == '31':
+            factor = [3, 7, 13, 17, 19, 23, 29, 37, 41, 43, 47, 53, 59, 67, 71]
+            rut_ajustado=str(rut).rjust( 15, '0')
+            total = sum(int(rut_ajustado[14-i]) * factor[i] for i in range(14)) % 11
+            if total > 1:
+                result =  11 - total
+            else:
+                result = total
+        else:
+            result = 0
+
+        return result
